4be12ce32a91f1b10ce4eb7f44d9a2b8d6191c98
lucascomputacao <lucascomputacao@gmail.com>
2016-08-12 14:29:26 -0300

    atualização de código

--- lua_code.lua
+++ lua_code.lua
@@ -1,9 +1,9 @@
 -- código exemplo para testes
 function fact(n)  -- isso é um comentário
-   variavel = 1; -- teste de variável
+   _variavel1223 = 1; -- teste de variável
    if n == 0 then -- isso é outro comentário
       return 20/2 -- alterado para testar divisão
    else        -- isso aind é comentário
-      return n * fact(n - 1) -- isso já tá ficando chato, mas é comentário também
+      return n * fact(n - 1); -- isso já tá ficando chato, mas é comentário também
    end						-- Eita que tem muito código nesse comentário, viu?
 end							-- Affff!!! Até que enfim acabou!!!
