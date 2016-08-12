6c0cbd515071528c5cc57c3cfe0a8bdfe0ef25d0
lucascomputacao <lucascomputacao@gmail.com>
2016-08-12 13:47:18 -0300

    alterado para testar divisão

--- lua_code.lua
+++ lua_code.lua
@@ -2,7 +2,7 @@
 function fact(n)  -- isso é um comentário
    variavel = 1; -- teste de variável
    if n == 0 then -- isso é outro comentário
-      return 1 -- isso é mais um comentário
+      return 20/2 -- alterado para testar divisão
    else        -- isso aind é comentário
       return n * fact(n - 1) -- isso já tá ficando chato, mas é comentário também
    end						-- Eita que tem muito código nesse comentário, viu?
