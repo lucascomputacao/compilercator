aaaa0e518085cf311f84ec577f189d69d7ed7366
lucascomputacao <lucascomputacao@gmail.com>
2016-08-15 11:55:49 -0300

    Inserindo erros

--- lua_code.lua
+++ lua_code.lua
@@ -1,12 +1,15 @@
 -- código exemplo para testes
 function fact(n)  -- isso é um comentário
-   _variavel1223 = 1; -- teste de variável
+   local _variavel1223 = 1; -- teste de variável
+   local ab  = 2;
+   local _var = 3;
+   local i123 = 4;%
    if n == 0 then -- isso é outro comentário
       return 20/2 -- alterado para testar divisão
-   else        -- isso aind é comentário
-      return n * fact(n - 1); -- isso já tá ficando chato, mas é comentário também
+   else        -- isso aind é comentário %
+      return n * fact(n - 1); % -- isso já tá ficando chato, mas é comentário também
    end						-- Eita que tem muito código nesse comentário, viu?
-end							-- Affff!!! Até que enfim acabou!!!
+end						%  	-- Affff!!! Até que enfim acabou!!!
    and      not      or        elseif     while    do        function
    end      for      else      if         then     return    local   nil
 -- Tambem abaixo sao outros itens lexicos permitidos na linguagem:
